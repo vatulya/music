91026045429bcace2497fc0f15078b6e24081f70
vatulyadev <vatulya.dev@gmail.com>
2016-04-12 11:57:14 +0300

    Init project

---
+++ index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+
+app.set('view engine', 'jade');
+
+app.get('/', function (req, res) {
+    res.render('index', { title: 'Hey', message: 'Hello there!'});
+});
+
+app.listen(3000, function () {
+    console.log('Started');
+});
